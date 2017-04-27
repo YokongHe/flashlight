60e54f64a75b8a829ed7a240f578e30f8052a5da
Yokong <heyk@tcl.com>
2017-04-27 20:58:25 +0800

    加入声音

--- app/src/main/java/com/surpax/a/c.java
+++ app/src/main/java/com/surpax/a/c.java
@@ -4,42 +4,45 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.util.Log;
+
 import com.surpax.ledflashlight.FlashlightActivity;
+import com.tcl.hawk.myflashlight.R;
+
 import java.util.HashMap;
 
 public final class c {
-   private SoundPool a;
-   private HashMap b;
+   private SoundPool soundPool;
+   private HashMap hashMap;
 
    public c(Context var1) {
       Log.d("Surpax App", "soundManager constructor");
-      this.a = new SoundPool(2, 3, 0);
-      if(this.a != null) {
+      this.soundPool = new SoundPool(2, 3, 0);
+      if(this.soundPool != null) {
          Log.d("flashlightactivity", "load sound resource now.....");
-         this.b = new HashMap();
-         this.b.put(Integer.valueOf(1), Integer.valueOf(this.a.load(var1, 2130837702, 1)));
-         this.b.put(Integer.valueOf(2), Integer.valueOf(this.a.load(var1, 2130837505, 1)));
-         this.b.put(Integer.valueOf(3), Integer.valueOf(this.a.load(var1, 2130837504, 1)));
+         this.hashMap = new HashMap();
+         this.hashMap.put(Integer.valueOf(1), Integer.valueOf(this.soundPool.load(var1, R.raw.sound_toggle, 1)));
+         this.hashMap.put(Integer.valueOf(2), Integer.valueOf(this.soundPool.load(var1, R.raw.adjustment_move, 1)));
+         this.hashMap.put(Integer.valueOf(3), Integer.valueOf(this.soundPool.load(var1, R.raw.adjustment_end, 1)));
       }
 
    }
 
    public final void a() {
-      if(this.b != null) {
-         this.b.clear();
+      if(this.hashMap != null) {
+         this.hashMap.clear();
       }
 
-      this.b = null;
-      if(this.a != null) {
-         this.a.release();
+      this.hashMap = null;
+      if(this.soundPool != null) {
+         this.soundPool.release();
       }
 
-      this.a = null;
+      this.soundPool = null;
    }
 
    public final void a(int var1) {
       Log.d("flashlightactivity", "play now.....");
-      if(this.a != null) {
+      if(this.soundPool != null) {
          AudioManager var3 = (AudioManager)FlashlightActivity.getFlashlightActivityInstance().getSystemService(Context.AUDIO_SERVICE);
          int var2;
          if(var3 != null) {
@@ -49,7 +52,7 @@ public final class c {
             var2 = 0;
          }
 
-         this.a.play(((Integer)this.b.get(Integer.valueOf(var1))).intValue(), (float)var2, (float)var2, 1, 0, 1.0F);
+         this.soundPool.play(((Integer)this.hashMap.get(Integer.valueOf(var1))).intValue(), (float)var2, (float)var2, 1, 0, 1.0F);
       }
 
    }
